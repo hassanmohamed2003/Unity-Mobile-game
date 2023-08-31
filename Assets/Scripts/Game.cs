c91757ab0974c9746edc9897f5ca5efc28530e28
DESKTOP-TTSNCQC\Thijs <thijs3214@gmail.com>
2023-08-31 16:15:36 +0200

    Added new structure to hold data about a building piece

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,51 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEditor.MPE;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
     private int frameCounter = 0;
-    public BuildingBlock blockPrefab;
-    public BuildingBlockLong blockLongPrefab;
-    private Queue<BuildingPieceType> nextBuildingPieces;
-
+    private Queue<BuildingPiece> nextBuildingPieces;
     public float newBlockStartY;
+    
+    [Header("Prefabs")]
+    public GameObject blockPrefab;
+    public GameObject rectanglePrefab;
 
-    enum BuildingPieceType{
-        SQUARE,
-        RECTANGLE
-    };
+    private struct BuildingPiece{
+        public GameObject prefab; // Use Resources.Load() to find the prefab
+        public Color color; // Color of the block
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        List<BuildingPieceType> list = new()
+        List<BuildingPiece> pieceList = new();
+        BuildingPiece piece1 = new()
         {
-            BuildingPieceType.SQUARE,
-            BuildingPieceType.RECTANGLE,
-            BuildingPieceType.SQUARE,
-            BuildingPieceType.SQUARE,
-            BuildingPieceType.RECTANGLE,
-            BuildingPieceType.RECTANGLE
+            prefab = blockPrefab,
+            color = Color.white
         };
-        createPieceQueue(list);
+        BuildingPiece piece2 = new(){
+            prefab = rectanglePrefab,
+            color = Color.cyan
+        };
+        BuildingPiece piece3 = new(){
+            prefab = rectanglePrefab,
+            color = Color.green
+        };
+        BuildingPiece piece4 = new(){
+            prefab = blockPrefab,
+            color = Color.red
+        };
+        pieceList.Add(piece1);
+        pieceList.Add(piece2);
+        pieceList.Add(piece3);
+        pieceList.Add(piece4);
+        CreatePieceQueue(pieceList);
     }
 
-    void createPieceQueue(IEnumerable<BuildingPieceType> pieces){
-        nextBuildingPieces = new Queue<BuildingPieceType>(pieces);
+    void CreatePieceQueue(IEnumerable<BuildingPiece> pieces){
+        nextBuildingPieces = new Queue<BuildingPiece>(pieces);
     }
 
-    void spawnNextPiece(Vector2 position, Quaternion quaternion){
+    void SpawnNextPiece(Vector2 position, Quaternion rotation){
         if(nextBuildingPieces.Count > 0){
-            BuildingPieceType type = nextBuildingPieces.Dequeue();
-            if(type == BuildingPieceType.SQUARE){
-                Instantiate(blockPrefab, position, quaternion);
-            }
-            else if(type == BuildingPieceType.RECTANGLE){
-                Instantiate(blockLongPrefab, position, quaternion);
-            }
+            BuildingPiece piece = nextBuildingPieces.Dequeue();
+            GameObject object1 = Instantiate(piece.prefab, position, rotation);
+            SpriteRenderer renderer = object1.GetComponentInChildren<SpriteRenderer>();
+            renderer.color = piece.color;
         }
     }
 
@@ -54,8 +66,8 @@ public class Game : MonoBehaviour
     {
         frameCounter++;
         if(frameCounter == 600){
-            Vector2 newBlockStartPosition = new Vector2(0, 4);
-            spawnNextPiece(newBlockStartPosition, Quaternion.identity);           
+            Vector2 newBlockStartPosition = new Vector2(Random.Range(-2, 2), newBlockStartY);
+            SpawnNextPiece(newBlockStartPosition, Quaternion.identity);           
             frameCounter = 0;
         }
     }
