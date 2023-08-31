74d7a9948a51f3061647a8bf2b6ea227fefa95a0
DESKTOP-TTSNCQC\Thijs <thijs3214@gmail.com>
2023-08-31 15:34:37 +0200

    Added Queue for spawning blocks

--- Assets/Scripts/BuildingBlockLong.cs
+++ Assets/Scripts/BuildingBlockLong.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class BuildingBlock : MonoBehaviour
+public class BuildingBlockLong : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,25 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.MPE;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
     private int frameCounter = 0;
     public BuildingBlock blockPrefab;
+    public BuildingBlockLong blockLongPrefab;
+    private Queue<BuildingPieceType> nextBuildingPieces;
+
+    public float newBlockStartY;
+
+    enum BuildingPieceType{
+        SQUARE,
+        RECTANGLE
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<BuildingPieceType> list = new()
+        {
+            BuildingPieceType.SQUARE,
+            BuildingPieceType.RECTANGLE,
+            BuildingPieceType.SQUARE,
+            BuildingPieceType.SQUARE,
+            BuildingPieceType.RECTANGLE,
+            BuildingPieceType.RECTANGLE
+        };
+        createPieceQueue(list);
+    }
+
+    void createPieceQueue(IEnumerable<BuildingPieceType> pieces){
+        nextBuildingPieces = new Queue<BuildingPieceType>(pieces);
+    }
+
+    void spawnNextPiece(Vector2 position, Quaternion quaternion){
+        if(nextBuildingPieces.Count > 0){
+            BuildingPieceType type = nextBuildingPieces.Dequeue();
+            if(type == BuildingPieceType.SQUARE){
+                Instantiate(blockPrefab, position, quaternion);
+            }
+            else if(type == BuildingPieceType.RECTANGLE){
+                Instantiate(blockLongPrefab, position, quaternion);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         frameCounter++;
-        if(frameCounter == 300){
+        if(frameCounter == 600){
             Vector2 newBlockStartPosition = new Vector2(0, 4);
-            Instantiate(blockPrefab, newBlockStartPosition, Quaternion.identity);
+            spawnNextPiece(newBlockStartPosition, Quaternion.identity);           
             frameCounter = 0;
         }
     }
