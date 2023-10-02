086bc895377a35bbfabb64bf9bc9efd0ced31652
Hassan Mohamed <s1136140@student.hsleiden.nl>
2023-10-02 17:13:46 +0200

    wop: settinsg

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [Header("Sound")]
+    public Button SFX;
+    public Button Music;
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+    public void turnOffMusic()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
