ec7f5500697ea0665d5c5e3dd4a94b0e331cbc04
ThijsvanDijk <thijs3214@gmail.com>
2023-10-05 13:40:19 +0200

    Fix blue Athur

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,4 +6,8 @@ public static class GameState
 {
     public static bool IsEndless = true;
     public static int CurrentLevelID;
+    public static bool NoFail = false;
+    public static bool LockedCheckpoint = false;
+    public static bool DisableRope = false;
+    public static bool UnlockAllLevels = false;
 }
